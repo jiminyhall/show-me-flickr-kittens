356d319d50426a88c4c26f15861c61c53847b694
Jimmy Hall <jiminy.hall@gmail.com>
2018-01-29 21:34:42 +0000

    Made secure

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 console.log('Pull photos from Flickr');
 
-$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=kitten,cute&size=b&lang=en-us&format=json&jsoncallback=?", function(data) {
+$.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?tags=kitten,cute&size=b&lang=en-us&format=json&jsoncallback=?", function(data) {
 
   var count = 1;
   var div = document.getElementById("images");
   div.innerHTML = div.innerHTML + "<div class='row'>";
-  
+
   $.each(data.items, function(i, item) {
 
-    var image = (item.media.m); 
+    var image = (item.media.m);
     div.innerHTML = div.innerHTML + "<div class='col-md-3' style='background-image: url(" + image + ")'/>";
     console.log('Count: ' + count + ' count%4=' + count%4);
     if (count % 4 === 0) {
